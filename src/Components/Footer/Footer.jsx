a794785de78554f9628ab77b6fc509497227753b
Andrew Lee <andrewimg@gmail.com>
2020-07-23 20:45:44 -0700

    took out random name in footer (John I'm sure you know who I'm talking about)

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import './Footer.css';
 function Footer(){
     return(
         <footer>
-            <p id="copyright">Copyright© Gildred Racing | Configurator Developed by <a href= 'https://www.linkedin.com/in/johnharmon805/' >John Harmon</a> & <a href= 'https://www.linkedin.com/in/andrewdavidlee7/' >Andrew Lee</a> & Tracy Ding</p>
+            <p id="copyright">Copyright© Gildred Racing | Configurator Developed by <a href= 'https://www.linkedin.com/in/johnharmon805/' >John Harmon</a> & <a href= 'https://www.linkedin.com/in/andrewdavidlee7/' >Andrew Lee</a></p>
         </footer>
     )
 }
