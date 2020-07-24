f76570ac4afe84f06ea0b5abf300a0ea75e647da
Andrew Lee <andrewimg@gmail.com>
2020-07-23 18:55:58 -0700

    Updates

---
+++ precache-manifest.1563cc223d14c73c088bee167b8e8a8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3d24c98841404a691a684fdf797e27f",
+    "url": "/car-configurator--pt2/index.html"
+  },
+  {
+    "revision": "7f48792fe056ed784a76",
+    "url": "/car-configurator--pt2/static/css/main.d71c8af2.chunk.css"
+  },
+  {
+    "revision": "8ee138e9fb6835645dd9",
+    "url": "/car-configurator--pt2/static/js/2.59ce3701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/car-configurator--pt2/static/js/2.59ce3701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f48792fe056ed784a76",
+    "url": "/car-configurator--pt2/static/js/main.6dc13938.chunk.js"
+  },
+  {
+    "revision": "9152f2f367a1b9b352d0",
+    "url": "/car-configurator--pt2/static/js/runtime-main.61ca5ded.js"
+  }
+]);
